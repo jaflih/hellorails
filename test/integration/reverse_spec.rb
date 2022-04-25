d82ad0bd50fe24fa62d95093485a6fd856515e00
jihaneh <jijih@hotmail.fr>
2022-04-25 21:40:02 +0200

    add test

---
+++ test/integration/reverse_spec.rb
@@ -0,0 +1,7 @@
+
+  context 'When testing the Solver class, reverse method' do
+    it 'it should return an empty string when an empty string is given ' do
+      expect(1).to eq ''
+    end
+  end
+
