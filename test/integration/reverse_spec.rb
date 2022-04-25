694dde4459ed6c7810ac8bbda7feb8cc155bf9b4
jihaneh <jijih@hotmail.fr>
2022-04-25 21:41:38 +0200

    fix linter

--- test/integration/reverse_spec.rb
+++ test/integration/reverse_spec.rb
@@ -1,7 +1,5 @@
-
-  context 'When testing the Solver class, reverse method' do
-    it 'it should return an empty string when an empty string is given ' do
-      expect(1).to eq ''
-    end
+context 'When testing the Solver class, reverse method' do
+  it 'it should return an empty string when an empty string is given ' do
+    expect(1).to eq ''
   end
-
+end
